065a65ce9bd8f39c59780f233356655da8dc35cb
Thrandur20 <tudor.dodonete@gmail.com>
2020-06-19 17:46:56 +0100

    created Publisher and added the Author and the Book Entities as well as the repositories for them. Also added a bootstrap class which is a CommandLineRunner in order to check if things work as intended

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -0,0 +1,53 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,
+                         PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author tudor = new Author("Tudor", "Dodonete");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        tudor.getBooks().add(ddd);
+        ddd.getAuthors().add(tudor);
+
+        authorRepository.save(tudor);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "123456789");
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started Bootstrap");
+        System.out.println("Number of books: " + bookRepository.count());
+
+        Publisher publisher = new Publisher("Jim Jones", "4 Wapping Lane, London, E1W 2RG");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Number of publishers: " + publisherRepository.count());
+    }
+}
